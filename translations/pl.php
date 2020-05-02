30e4f2cb0f8bf0e711bc9fe1979a166e7b601dca
cienislaw <cienislaw@post.pl>
2020-05-02 22:12:23 +0200

    Added Polish translation

---
+++ translations/pl.php
@@ -0,0 +1,10 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{loginas}prestashop>loginas_d97f0bac6428cc1d248d2de0d932930e'] = 'Zaloguj się jako klient';
+$_MODULE['<{loginas}prestashop>loginas_121dfb5ef16ff8a3525332da29707dec'] = 'Moudł umożliwia zalogowanie się w sklepie jako wybrany klient';
+$_MODULE['<{loginas}prestashop>unauthorized_e06d1ba70f1331e9f9a113cc2f887d3f'] = 'Brak uprawnień';
+$_MODULE['<{loginas}prestashop>unauthorized_59212173c895a8d89663b5f7c4e529e6'] = 'Nie masz uprawnień, aby użyć tej funkcji';
+$_MODULE['<{loginas}prestashop>customer-form_9698fd0abac915053f837516d2c73127'] = 'Zaloguj się jako klient';
+$_MODULE['<{loginas}prestashop>customer-form_9bdca10f4bf5b9ac53e10e5da5e7711f'] = 'Zaloguj się jako %s';
